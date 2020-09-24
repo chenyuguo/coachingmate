1b075274ca538252c2afd2f5b9419574f4540e57
ShuaiMou <shuaimou77@gmail.com>
2020-09-24 12:12:41 +1000

    The ActivityDao test passed

---
+++ src/test/java/coachingmateanalytics/coachingmate/dao/ActivityDaoImplTest.java
@@ -0,0 +1,41 @@
+package coachingmateanalytics.coachingmate.dao;
+
+import coachingmateanalytics.coachingmate.AppTest;
+import coachingmateanalytics.coachingmate.UserDaoTest;
+import coachingmateanalytics.coachingmate.entity.Activity;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Date: 24/9/20 12:04
+ * @Description:
+ */
+class ActivityDaoImplTest extends AppTest {
+    private static final Logger logger = LoggerFactory.getLogger(UserDaoTest.class);
+
+    @Autowired
+    private ActivityDao activityDao;
+
+    @Test
+    void saveActivity() {
+        Activity activity = new Activity("3f3d5af3-7847-413e-a9fe-47aeffb6de44", "testData2");
+        activityDao.saveActivity(activity);
+    }
+
+    @Test
+    void findAllByAccessToken() {
+        List<Activity> allByAccessToken = activityDao.findAllByAccessToken("3f3d5af3-7847-413e-a9fe-47aeffb6de44");
+        int i = 1;
+        for (Activity activity : allByAccessToken) {
+            logger.info(activity.getAccessToken() + i + " : " + activity.getData());
+        }
+
+        logger.info("total count : " + allByAccessToken.size());
+    }
+}
